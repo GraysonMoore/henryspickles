828e98c8018cf6a36c7ae0d7fc6eff8c863e1825
GraysonMoore <moor8456@hotmail.com>
2021-04-05 15:24:48 -0700

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,6 +1,7 @@
 <?php
-	if (false) {
-		/*$name = $_POST['name'];
+	$udata = data;
+	if (true) {
+		$name = $_POST['name'];
 		$email = $_POST['email'];
 		$address = $_POST['address'];
 		$city = $_POST['city'];
@@ -12,26 +13,11 @@
 		$type1 = $_POST['type1'];
 		$type2 = $_POST['type2'];
 		$type3 = $_POST['type3'];
-		$pickup = $_POST['pickup'];*/
-
-		$name = ['name=name'];
-		$email = ['name=email'];
-		$address = ['name=address'];
-		$city = ['name=city'];
-		$state = ['name=state'];
-		$comments = ['name=comments'];
-		$amount1 = ['name=amount1'];
-		$amount2 = ['name=amount2'];
-		$amount3 = ['name=amount3'];
-		$type1 = ['name=type1'];
-		$type2 = ['name=type2'];
-		$type3 = ['name=type3'];
-		$pickup = ['name=pickup'];
+		$pickup = $_POST['pickup'];
 		
 		$header = "Content-Type: text/html\r\nReply-To: $email\r\nFrom: $name <$email>";
 
-		$body =
-		@"Email sent from ".$_SERVER['REMOTE_ADDR']." at ".date("d/m/Y H:1",time())."<br />
+		$body = @"Email sent from ".$_SERVER['REMOTE_ADDR']." at ".date("d/m/Y H:1",time())."<br />
 		<hr />
 		$amount1
 		$type1
@@ -47,6 +33,6 @@
 		<hr />
 		Email end";
 
-		mail("ghmmoore@gmail.com", "A user sent you an email", $body, $header);
+		mail("ghmmoore@gmail.com", "You have an order!", $body, $header);
 	}
 ?>
