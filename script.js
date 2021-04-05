ea489ae918a2ff10b62c461c4a3d1c06cd32d02b
GraysonMoore <moor8456@hotmail.com>
2021-04-05 15:38:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ $("#calculate").click(function(event) {
 		var quantity1 = Number($("#amount1").val());
 		var quantity2 = Number($("#amount2").val());
 		var quantity3 = Number($("#amount3").val());
-		var data = false;
+		
 
 		if(quantity1 > 20 || quantity2 > 20 || quantity3 > 20) {
 			alert("I know pickles are good, but you can only buy 20 jars at once");
@@ -149,9 +149,8 @@ $("#calculate").click(function(event) {
 			alert("Form not completed");
 		} else {
 			$("#sendEmail").text("Sending...");
-			data = true;
 			
-			/*$.post("sendemail.php", {
+			$.post("sendemail.php", {
 				name: userName, email: userEmail, address: userAddress, city: userCity, state: userState, comments: userComments, amount1: userAmount1, amount2: userAmount2, amount3: userAmount3, pickup: userPickup, type1: userType1, type2: userType2, type3: userType3
 			}, function(data) {
 				if(data == "true") {
@@ -159,7 +158,7 @@ $("#calculate").click(function(event) {
 				} else {
 					$("#sendEmail").text("Send");
 				}
-			});*/
+			});
 		}
 		
 	});
