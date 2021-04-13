cc2ee2a3d3fd037dae0ffcddda32525d76d7e801
GraysonMoore <moor8456@hotmail.com>
2021-04-13 10:02:20 -0700

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,25 +1,7 @@
-	
-		
-
-		<?php    if (isset($_REQUEST['email']))  {      
-			$admin_email = "ghmmoore@gmail.com";   
-			$email = $_REQUEST['email'];   
-			$subject = $_REQUEST['subject'];   
-			$comment = $_REQUEST['comment'];      
-			mail($admin_email, "$subject", $comment, "From:" . $email);      
-			
-			echo "Thank you for contacting us!";   }  else  { ?>   <form method="post">
-	
-   			Email: <input name="email" type="text" />
-   			Subject: <input name="subject" type="text" />
-   			Message:
-   			<textarea name="comment" rows="15" cols="40"></textarea>
-   			<input type="submit" value="Submit" />   </form>    <?php   } ?>
-
 <?php
-		/*$name = $_POST['name'];
-		$email = $_POST['email'];
-		$msg = $_POST['comments'];
+		$name = $_GET['name'];
+		$email = $_GET['email'];
+		$msg = $_GET['comments'];
 		
 		$to = "ghmmoore@gmail.com";
 		$subject = "You have an order!";
@@ -31,7 +13,7 @@
 		echo "The email sent!";
 		
 		
-		$address = $_POST['address'];
+		/*$address = $_POST['address'];
 		$city = $_POST['city'];
 		$state = $_POST['state'];
 		$comments = $_POST['comments'];
